e7d19bd3b3be1414fd3b6b2033fcce77b6997644
ahhhhhhhhhhhhhhhhhhhhhh1 <egarots6389@gmail.com>
2026-05-26 21:23:15 +0930

    Update stuff.py

--- stuff.py
+++ stuff.py
@@ -1,5 +1,10 @@
 import os
-import subprocess
+import requests
 
-if not os.path.exists(r"C:\positron"):
-    subprocess.run("curl \"\"")
+if not os.path.exists(r"C:\Program Files\positron"):
+    import requests
+
+    url = ""
+    response = requests.get(url)
+    
+    print(response.text)
